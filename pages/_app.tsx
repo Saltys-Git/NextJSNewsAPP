3de2f2ea69459c7b2da6786082e62edb6e520ea8
Saltys-Git <sidurrahmansiam@gmail.com>
2023-04-12 19:09:16 +0600

    third

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NextNProgress/>
+      {/* <NextNProgress/> */}
       <NavBar/>
       <Container className={styles.pageContainer}>
         <Component {...pageProps} />
